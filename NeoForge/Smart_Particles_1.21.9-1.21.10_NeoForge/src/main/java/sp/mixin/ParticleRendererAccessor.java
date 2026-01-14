04dc7a0de6a596d7f759e7654f8709bee89b4f6c
chedidandrew <145610849+chedidandrew@users.noreply.github.com>
2026-01-13 21:59:59 -0600

    Smart Particles v21.10.9 - NeoForge Release

--- NeoForge/Smart_Particles_1.21.9-1.21.10_NeoForge/src/main/java/sp/mixin/ParticleRendererAccessor.java
+++ NeoForge/Smart_Particles_1.21.9-1.21.10_NeoForge/src/main/java/sp/mixin/ParticleRendererAccessor.java
@@ -7,9 +7,6 @@ import org.spongepowered.asm.mixin.gen.Accessor;
 
 import java.util.Map;
 
-/**
- * In 1.21.9 this map's value type is not a Queue, so keep it as Object and handle dynamically.
- */
 @Mixin(ParticleEngine.class)
 public interface ParticleRendererAccessor {
     @Accessor("particles")
