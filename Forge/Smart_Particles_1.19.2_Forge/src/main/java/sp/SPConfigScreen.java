930e364e507efa3a54526fdd2f8ac6a6c4b895b0
chedidandrew <145610849+chedidandrew@users.noreply.github.com>
2026-02-04 11:47:26 -0600

    1.19.2 Forge Release

--- Forge/Smart_Particles_1.19.2_Forge/src/main/java/sp/SPConfigScreen.java
+++ Forge/Smart_Particles_1.19.2_Forge/src/main/java/sp/SPConfigScreen.java
@@ -8,13 +8,12 @@ import net.minecraft.client.gui.components.EditBox;
 import net.minecraft.client.gui.screens.ConfirmLinkScreen;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
-import net.minecraft.client.gui.components.Button;
-import net.minecraft.network.chat.Component;
 
 import java.net.URI;
 
 public class SPConfigScreen extends Screen {
     private final Screen parent;
+
     private EditBox limitField;
     private CycleButton<Boolean> cullingButton;
     private boolean smartCulling;
@@ -29,20 +28,55 @@ public class SPConfigScreen extends Screen {
     protected void init() {
         super.init();
 
-        // Reset Button
+        // 1. Smart Culling Toggle (1.19.2: booleanBuilder instead of onOffBuilder)
+        this.cullingButton = CycleButton.booleanBuilder(
+                        Component.literal("ON"),
+                        Component.literal("OFF")
+                )
+                .withInitialValue(this.smartCulling)
+                .create(
+                        this.width / 2 - 100,
+                        this.height / 4 + 24,
+                        200,
+                        20,
+                        Component.literal("Smart Culling"),
+                        (button, value) -> this.smartCulling = value
+                );
+        this.addRenderableWidget(this.cullingButton);
+
+        // 2. Particle Limit Text Field
+        this.limitField = new EditBox(
+                this.font,
+                this.width / 2 - 100,
+                this.height / 4 + 48 + 20,
+                200,
+                20,
+                Component.literal("Particle Limit")
+        );
+        this.limitField.setValue(String.valueOf(SPConfig.instance.particleLimit));
+        this.limitField.setFilter(text -> text.matches("\\d*"));
+        this.addRenderableWidget(this.limitField);
+
+        // 3. Reset Button
         this.addRenderableWidget(new Button(
-                this.width / 2 - 100, this.height - 88, 200, 20,
+                this.width / 2 - 100,
+                this.height - 88,
+                200,
+                20,
                 Component.literal("Reset to Defaults"),
                 button -> {
                     this.smartCulling = true;
-                    this.cullingButton.setValue(true);
-                    this.limitField.setValue("5000");
+                    if (this.cullingButton != null) this.cullingButton.setValue(true);
+                    if (this.limitField != null) this.limitField.setValue("5000");
                 }
         ));
 
-        // Save & Quit Button
+        // 4. Save & Quit Button
         this.addRenderableWidget(new Button(
-                this.width / 2 - 100, this.height - 64, 200, 20,
+                this.width / 2 - 100,
+                this.height - 64,
+                200,
+                20,
                 Component.literal("Save & Quit"),
                 button -> {
                     save();
@@ -50,26 +84,35 @@ public class SPConfigScreen extends Screen {
                 }
         ));
 
-        // Link Buttons (Ko-fi | Modrinth | Curseforge)
+        // 5. Link Buttons (Ko-fi | Modrinth | Curseforge)
         int buttonWidth = 64;
         int spacing = 4;
         int startX = this.width / 2 - ((buttonWidth * 3) + (spacing * 2)) / 2;
         int yPos = this.height - 30;
 
         this.addRenderableWidget(new Button(
-                startX, yPos, buttonWidth, 20,
+                startX,
+                yPos,
+                buttonWidth,
+                20,
                 Component.literal("Ko-fi"),
                 b -> openLink("https://ko-fi.com/andrewchedid")
         ));
 
         this.addRenderableWidget(new Button(
-                startX + buttonWidth + spacing, yPos, buttonWidth, 20,
+                startX + buttonWidth + spacing,
+                yPos,
+                buttonWidth,
+                20,
                 Component.literal("Modrinth"),
                 b -> openLink("https://modrinth.com/mod/smart-particles")
         ));
 
         this.addRenderableWidget(new Button(
-                startX + (buttonWidth + spacing) * 2, yPos, buttonWidth, 20,
+                startX + (buttonWidth + spacing) * 2,
+                yPos,
+                buttonWidth,
+                20,
                 Component.literal("Curse"),
                 b -> openLink("https://www.curseforge.com/minecraft/mc-mods/smart-particles")
         ));
@@ -84,7 +127,6 @@ public class SPConfigScreen extends Screen {
     }
 
     private void openLink(String url) {
-        // Opens the standard "Are you sure you want to open this link?" screen
         this.minecraft.setScreen(new ConfirmLinkScreen((yes) -> {
             if (yes) {
                 Util.getPlatform().openUri(URI.create(url));
@@ -110,12 +152,12 @@ public class SPConfigScreen extends Screen {
 
         drawCenteredString(poseStack, this.font, this.title, this.width / 2, 20, 0xFFFFFF);
         drawCenteredString(
-            poseStack,
-            this.font,
-            Component.literal("Particle Limit:"),
-            this.width / 2,
-            this.height / 4 + 48 + 5,
-            0xA0A0A0
+                poseStack,
+                this.font,
+                Component.literal("Particle Limit:"),
+                this.width / 2,
+                this.height / 4 + 48 + 5,
+                0xA0A0A0
         );
     }
 
